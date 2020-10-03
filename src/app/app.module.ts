0880f2b40f95ead0a6c217d2680f4b3b2551eca3
sumanmandal <suman.mandal@obopay.com>
2020-10-03 20:16:06 +0530

    https client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule }            from '@angular/platform-browser'
+import { NgModule }                 from '@angular/core'
+import { AppRoutingModule }         from './app-routing.module'
+import { AppComponent }             from './app.component'
+import { BrowserAnimationsModule }  from '@angular/platform-browser/animations'
+import { HttpClientModule }         from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,6 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
