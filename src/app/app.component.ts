6570b5dbacfc05123f7ff3fee2753f4b03572553
sumanmandal <suman.mandal@obopay.com>
2020-10-14 08:47:09 +0530

    small chnages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { ClientStorageService } from './framework/storage-provider/client-storage.service';
+import { Router } from '@angular/router';
+import { COMPONENTS_ROUTES } from './framework/app-routing-constant';
 
 @Component({
   selector    : 'app-root',
@@ -11,11 +13,15 @@ export class AppComponent implements OnInit{
   
   userId  : string
 
-  constructor(private storage : ClientStorageService) {
+  constructor(private storage : ClientStorageService,
+              private router  : Router) {
   }
 
   ngOnInit(): void {
     this.userId = this.storage.getLocalStorage('userId')
+    if (this.userId)  {
+      this.router.navigate([COMPONENTS_ROUTES.LandingComp])
+    }
   }
 
 
